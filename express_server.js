661db843738e599e497ede634395a7caf41d7574
Jan Edmark Arieta <edarieta09@gmail.com>
2024-01-15 21:52:58 +0000

    add edit button and functionality

--- express_server.js
+++ express_server.js
@@ -71,3 +71,9 @@ app.post("/urls/:id/delete", (req, res) => {
   delete urlDatabase[id];
   res.redirect("/urls");
 });
+
+app.post("/urls/:id", (req, res) => {
+  const id = req.params.id;
+  urlDatabase[id] = req.body.longURL;
+  res.redirect("/urls");
+});
