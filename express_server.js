874388fff49a33f08a1f2c5e07da7197de9f7260
Jan Edmark Arieta <edarieta09@gmail.com>
2024-01-15 15:16:33 +0000

    add views dir

--- express_server.js
+++ express_server.js
@@ -7,6 +7,7 @@ app.set("view engine", "ejs");
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
+
 };
 
 app.get("/", (req, res) => {
@@ -22,6 +23,12 @@ app.get("/urls.json", (req, res) => {
 });
 
 app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
+  const templateVars = { greeting: "Hello World!" };
+  res.render("hello_world", templateVars);
+});
+
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
 });
 
