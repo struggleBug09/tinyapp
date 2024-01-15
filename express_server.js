7c9aa7368a281da43082e8f23174195f360db8db
Jan Edmark Arieta <edarieta09@gmail.com>
2024-01-15 00:26:45 +0000

    check curl and hello world html

--- express_server.js
+++ express_server.js
@@ -5,7 +5,6 @@ const PORT = 8080; // default port 8080
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
-  "xyz": "some website"
 };
 
 app.get("/", (req, res) => {
@@ -16,6 +15,10 @@ app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
-app.get("/urllinks.json", (req, res) => {
+app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
+});
+
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
