2b7cc87a223e32ff24c974bee0aaa05b76088a61
Jan Edmark Arieta <edarieta09@gmail.com>
2024-01-15 20:19:09 +0000

    add shortURL support, implement redirect

--- express_server.js
+++ express_server.js
@@ -48,13 +48,21 @@ app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
+app.post("/urls", (req, res) => {
+  const id = generateRandomString();
+  const longURL = req.body.longURL;
+  urlDatabase[id] = longURL;
+  res.redirect(`/urls/${id}`);
+});
+
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
 });
 
-app.post("/urls", (req, res) => {
-  console.log(req.body); 
-  res.send("Ok"); 
-});
 
+app.get("/u/:id", (req, res) => {
+  const id = req.params.id;
+  const longURL = urlDatabase[id];
+  res.redirect(longURL);
+});
