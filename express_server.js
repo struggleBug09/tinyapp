ae5a2cdfb04efcf0e82ea5b2c7746b3f0b6bc601
Jan Edmark Arieta <edarieta09@gmail.com>
2024-01-23 14:25:23 +0000

    add login functionality

--- express_server.js
+++ express_server.js
@@ -77,3 +77,9 @@ app.post("/urls/:id", (req, res) => {
   urlDatabase[id] = req.body.longURL;
   res.redirect("/urls");
 });
+
+app.post("/login", (req, res) => {
+  const username = req.body.username;
+  res.cookie("username", username);
+  res.redirect("/urls");
+});
